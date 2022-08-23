685b08b2c8efc5aa839766c6d42b3e5ef2390ab8
Mystaras <d.soupilas@gmail.com>
2022-08-23 04:14:07 +0200

    small opt

--- scripts/pyenv_info.sh
+++ scripts/pyenv_info.sh
@@ -5,7 +5,7 @@ cd $PANE_PATH
 PYENV_GLOB="$(pyenv global)"
 PYENV_LOC="$(pyenv local)"
 
-if ! [ $PYENV_GLOB = "system" ] || ! [ $PYENV_LOC = "system" ]; then
+if [ $PYENV_GLOB != "system" ] || [ $PYENV_LOC != "system" ]; then
     OUTPUT="#[bold]🐍env #[nobold]"
 
     if ! [ $PYENV_GLOB = "system" ]; then
